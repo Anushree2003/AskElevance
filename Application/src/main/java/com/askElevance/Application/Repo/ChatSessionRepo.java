9cf4d2ecc95e532b097d1bc94f1ffdad83cc3586
AnushreeSinha <AnushreeSinha@IBM-589XGY3.in.ibm.com>
2026-02-24 16:54:20 +0530

    added chatsession repo

---
+++ Application/src/main/java/com/askElevance/Application/Repo/ChatSessionRepo.java
@@ -0,0 +1,12 @@
+package com.askElevance.Application.Repo;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.askElevance.Application.Entity.ChatSession;
+import com.askElevance.Application.Entity.User;
+
+public interface ChatSessionRepo extends JpaRepository<ChatSession, Long>{
+	List<ChatSession> findByUser(User user);
+}
