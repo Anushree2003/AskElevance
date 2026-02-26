41b0a9815ea3d45f8159658db57ff4afce0a6167
AnushreeSinha <AnushreeSinha@IBM-589XGY3.in.ibm.com>
2026-02-26 15:41:16 +0530

    implemented llm api for response

--- Application/src/main/java/com/askElevance/Application/Controller/AuthController.java
+++ Application/src/main/java/com/askElevance/Application/Controller/AuthController.java
@@ -18,33 +18,33 @@ import com.askElevance.Application.Service.AuthService;
 @RequestMapping("/auth")
 
 public class AuthController {
-	 @Autowired
-	    private AuthService authService;
-
-	    // Register API
-	    @PostMapping("/register")
-	    public ResponseEntity<String> register(
-	            @RequestBody RegisterRequest request) {
-
-	        authService.register(request);
-	        return ResponseEntity.ok("User Registered Successfully");
-	    }
-	    
-
-	    // Login API
-	    @PostMapping("/login")
-	    public ResponseEntity<AuthResponse> login(
-	            @RequestBody LoginRequest request) {
-
-	        AuthResponse response =
-	                authService.login(request);
-
-	        return ResponseEntity.ok(response);
-	    }
-
-	    // Health Check
-	    @GetMapping("/check")
-	    public String check() {
-	        return "Auth Working";
-	    }
+//	 @Autowired
+//	    private AuthService authService;
+//
+//	    // Register API
+//	    @PostMapping("/register")
+//	    public ResponseEntity<String> register(
+//	            @RequestBody RegisterRequest request) {
+//
+//	        authService.register(request);
+//	        return ResponseEntity.ok("User Registered Successfully");
+//	    }
+//	    
+//
+//	    // Login API
+//	    @PostMapping("/login")
+//	    public ResponseEntity<AuthResponse> login(
+//	            @RequestBody LoginRequest request) {
+//
+//	        AuthResponse response =
+//	                authService.login(request);
+//
+//	        return ResponseEntity.ok(response);
+//	    }
+//
+//	    // Health Check
+//	    @GetMapping("/check")
+//	    public String check() {
+//	        return "Auth Working";
+//	    }
 }
--- Application/src/main/java/com/askElevance/Application/Service/AuthService.java
+++ Application/src/main/java/com/askElevance/Application/Service/AuthService.java
@@ -19,47 +19,47 @@ import com.askElevance.Application.Repo.UserRepo;
 
 @Service
 public class AuthService {
-	 @Autowired
-	    private UserRepo userRepository;
-
-	    @Autowired
-	    private PasswordEncoder passwordEncoder;
-
-	    @Autowired
-	    private AuthenticationManager authenticationManager;
-
-	    // REGISTER USER
-	    public void register(RegisterRequest request) {
-
-	        User user = new User();
-
-	        user.setName(request.getName());
-
-	        user.setPassword(
-	                passwordEncoder.encode(request.getPassword())
-	        );
-
-	       
-	        userRepository.save(user);
-	    }
-
-	    // LOGIN USER
-	    public AuthResponse login(LoginRequest request) {
-
-	        authenticationManager.authenticate(
-	                new UsernamePasswordAuthenticationToken(
-	                        request.getEmail(),
-	                        request.getPassword()
-	                )
-	        );
-
-	        // Later you can generate JWT token here
-
-	        return new AuthResponse(
-	                "Login Successful"
-	        );
-	        
-	    }
+//	 @Autowired
+//	    private UserRepo userRepository;
+//
+//	    @Autowired
+//	    private PasswordEncoder passwordEncoder;
+//
+//	    @Autowired
+//	    private AuthenticationManager authenticationManager;
+//
+//	    // REGISTER USER
+//	    public void register(RegisterRequest request) {
+//
+//	        User user = new User();
+//
+//	        user.setName(request.getName());
+//
+//	        user.setPassword(
+//	                passwordEncoder.encode(request.getPassword())
+//	        );
+//
+//	       
+//	        userRepository.save(user);
+//	    }
+//
+//	    // LOGIN USER
+//	    public AuthResponse login(LoginRequest request) {
+//
+//	        authenticationManager.authenticate(
+//	                new UsernamePasswordAuthenticationToken(
+//	                        request.getEmail(),
+//	                        request.getPassword()
+//	                )
+//	        );
+//
+//	        // Later you can generate JWT token here
+//
+//	        return new AuthResponse(
+//	                "Login Successful"
+//	        );
+//	        
+//	    }
 	
 
 }
